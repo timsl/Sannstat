a24cfb71b402b93f6f73d38e711162a83805627b
timsl <tiol@kth.se>
2015-03-03 23:01:06 +0100

    labb 2 except linear regression

--- problem2.m
+++ problem2.m
@@ -1,6 +1,6 @@
 function [ output_args ] = problem2( input_args )
 
-mux = 0; muy = 100; sigmax = 1; sigmay = 4; rho = 0.7;
+mux = 0; muy = 1; sigmax = 1; sigmay = 1; rho = -0.8;
 plot_mvnpdf(mux, muy, sigmax, sigmay, rho)
 
 end
